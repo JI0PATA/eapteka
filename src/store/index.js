dbece2ad76ff327ed9405291cfbf31bad069231d
JI0PATA <wanja.shukov14@gmail.com>
2021-05-30 11:56:36 +0300

    Pseudo code for pushs

--- src/store/index.js
+++ src/store/index.js
@@ -35,6 +35,48 @@ export default new Vuex.Store({
     },
     closeCatalogCard({ commit }) {
       commit('setCatalogCard', null)
+    },
+    getNotify({ commit }) {
+      importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
+      importScripts(
+          "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+      );
+// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+      importScripts(
+          "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+      );
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+      firebase.initializeApp({
+        messagingSenderId: "1073231139868",
+        apiKey: "AIzaSyDlqdLsjc1uBuQT4pRTl36en4IjvAgU9zA",
+        projectId: "eapteka-hack-2021",
+        appId: "1:1073231139868:web:93f77654e44a9832f63bd9"
+      });
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+      const messaging = firebase.messaging();
+
+      messaging.setBackgroundMessageHandler(function(payload) {
+        console.log(
+            "[firebase-messaging-sw.js] Received background message ",
+            payload,
+        );
+        // Customize notification here
+        const notificationTitle = "Background Message Title";
+        const notificationOptions = {
+          body: "Background Message body.",
+          icon: "/itwonders-web-logo.png",
+        };
+
+        return self.registration.showNotification(
+            notificationTitle,
+            notificationOptions,
+        );
+      });
+
     }
   },
   getters: {
