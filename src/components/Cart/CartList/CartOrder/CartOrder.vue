e6c957ee0730661507f37a0989b4268595e3e5b1
JI0PATA <wanja.shukov14@gmail.com>
2021-05-30 07:51:47 +0300

    Full cart page

---
+++ src/components/Cart/CartList/CartOrder/CartOrder.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="CartOrder">
+        <div class="CartOrder--property">
+            <div class="CartOrder--title">Ямашева проспект, 19</div>
+            <div class="CartOrder--arrow">
+                <img src="@/assets/img/icons/chevron-down.svg" alt="Chrevron">
+            </div>
+        </div>
+        <div class="CartOrder--property">
+            <div class="CartOrder--title">Оплата SberPay</div>
+            <div class="CartOrder--arrow">
+                <img src="@/assets/img/icons/chevron-down.svg" alt="Chrevron">
+            </div>
+        </div>
+        <div class="CartOrder--line"></div>
+        <div class="CartOrder--subtitle">
+            <span>Товары с учётом скидки</span>
+            <span>1240 ₽</span>
+        </div>
+        <div class="CartOrder--subtitle">
+            <span>Доставка за 15 минут</span>
+            <span>0 ₽</span>
+        </div>
+        <div class="CartOrder--subtitle CartOrder--subtitle-black">
+            <span>К оплате</span>
+            <span>1240 ₽</span>
+        </div>
+        <button class="CartOrder--btn">Заказать</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CartOrder"
+}
+</script>
+
+<style lang="scss">
+@import "CartOrder";
+</style>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -4,18 +4,49 @@
                 title="Корзина"
         />
         <CartList />
+        <Catalog>
+            <CatalogHeader>
+                <CatalogTitle>Ничего не забыли</CatalogTitle>
+            </CatalogHeader>
+            <CatalogList>
+                <CatalogItem
+                        v-for="(product, index) in recommendationsProducts"
+                        :key="index"
+                        :product="product"
+                />
+            </CatalogList>
+        </Catalog>
+        <CartOrder />
     </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex"
 import GroupProductsHeader from "@/components/Header/GroupProductsHeader/GroupProductsHeader"
-import CartList from "@/components/Cart/CartList/CartList";
+import CartList from "@/components/Cart/CartList/CartList"
+import Catalog from "@/components/Catalog/Catalog"
+import CatalogHeader from "@/components/Catalog/CatalogHeader/CatalogHeader"
+import CatalogTitle from "@/components/Catalog/CatalogTitle/CatalogTitle"
+import CatalogMore from "@/components/Catalog/CatalogMore/CatalogMore"
+import CatalogList from "@/components/Catalog/CatalogList/CatalogList"
+import CatalogItem from "@/components/Catalog/CatalogList/CatalogItem/CatalogItem"
+import CartOrder from "@/components/Cart/CartList/CartOrder/CartOrder";
 
 export default {
     name: "Cart",
     components: {
         GroupProductsHeader,
-        CartList
+        CartList,
+        Catalog,
+        CatalogHeader,
+        CatalogTitle,
+        CatalogMore,
+        CatalogList,
+        CatalogItem,
+        CartOrder
+    },
+    computed: {
+        ...mapGetters(['recommendationsProducts'])
     }
 }
 </script>
